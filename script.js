68d3bade5daae0cca15d058933dbb3600cf6d8e5
Gugamk <dekaneidze@gmail.com>
2024-06-02 23:32:57 +0400

    untill 7

---
+++ script.js
@@ -0,0 +1,79 @@
+// 1
+
+// for (let i = 5; i < 100; i++) {
+//   console.log(i);
+// }
+
+// 2
+
+// let array1 = [1, 2, 4, -1, -3, 8, 7, 0, -5, 18, 12];
+
+// for (let i = 0; i < array1.length; i++) {
+//   let nestedArray = array1[i];
+//   if (nestedArray > 0 && nestedArray < 10) {
+//     console.log(nestedArray);
+//   }
+// }
+
+// for (let item of array1) {
+//   if (item > 0 && item < 10) {
+//     console.log(item);
+//   }
+// }
+
+// 3
+
+// let array2 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+// for (i = 0; i < array2.length; i++) {
+//   let specifictNumber = array2[i];
+//   if (specifictNumber === 5) {
+//     console.log(`theres 5`);
+//     break;
+//   }
+// }
+
+// for (let item of array2) {
+//   if (item === 5) {
+//     console.log(`theres 5 in this array`);
+//   }
+// }
+
+// 4
+
+// let array3 = [1, 2, 3, 4, 5];
+// let sumArray = 0;
+// for (let item of array3) {
+//   sumArray += item;
+// }
+// console.log(sumArray);
+
+// 5
+
+// let array4 = [1, 2, 3, 4, 5];
+// let sumArray = 0;
+// for (let item of array4) {
+//   sumArray += item;
+// }
+// const average = sumArray / array4.length;
+// console.log(average);
+
+// 6
+
+// let array5 = [1, 2, 3, 7, 6, 9];
+
+// for (let item of array5) {
+//   if (item == 7) {
+//     continue;
+//   }
+//   console.log(item);
+// }
+
+// 7
+
+let user = {
+  firstname: "giorgi",
+  lastname: "smith",
+  age: 25,
+  studentstatus: "active",
+};
